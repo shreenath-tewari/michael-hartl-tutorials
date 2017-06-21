efdcd6962efe844c73aa70fc306672b88dbc5c72
Shreenath Tewari <stewari.sps@gmail.com>
2017-06-21 20:57:50 +0530

    Bootstrap mockup added via navbar to application.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = "Sample App"
+    if page_title.empty?
+      return title
+    else
+      title = page_title + " | " + base_title
+    end
+    title
+  end
+
+
 end
