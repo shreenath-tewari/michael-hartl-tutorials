3fcf88a1e71365f93089129a72833ee7199a6ba4
Shreenath Tewari <stewari.sps@gmail.com>
2017-06-24 15:18:37 +0530

    Sign Up route configured and Controller test added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
+  get '/signup', to: "users#new"
 
   root "static_pages#home"
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @title = "Sample App"
+  end
+
   test "should get new" do
-    get users_new_url
+    get signup_path
     assert_response :success
+    assert_select "title", "Sign Up | #{@title}"
   end
 
 end
