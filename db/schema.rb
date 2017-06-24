7f56fbb77b24f87a3f38c929938e91a8a87d7e80
Shreenath Tewari <stewari.sps@gmail.com>
2017-06-24 16:25:42 +0530

    before_save callback added to User Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624094025) do
+ActiveRecord::Schema.define(version: 20170624105323) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
