29eb19288762603ddcd1f891beba2f8dc2fe1042
Shreenath Tewari <stewari.sps@gmail.com>
2017-06-27 18:29:00 +0530

    Sessions controller added and routes configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
 
+  # Routes for sessions_controller
+
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/login', to: "sessions#destroy"
+
+  # Routes for users_controller
+
   get '/signup', to: "users#new"
-
   post '/signup', to: "users#create"
-
   resources :users
 
+  # Routes for static_pages_controller
 
   root "static_pages#home"
-
   get '/help', to: "static_pages#help"
-
   get '/about', to: "static_pages#about"
-
   get '/contact', to: "static_pages#contact"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
